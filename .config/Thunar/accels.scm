dbb8b6d1fe89739e24472583dcd6313d6c302f2a
Jader Brasil <jaderebrasil@gmail.com>
2020-08-31 16:20:20 -0300

    termspawn minor fix
    now, termspawn vim {filename} {parameters} works.

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -1,4 +1,4 @@
-; Thunar GtkAccelMap rc-file         -*- scheme -*-
+; thunar GtkAccelMap rc-file         -*- scheme -*-
 ; this file is an automated accelerator map dump
 ;
 ; (gtk_accel_path "<Actions>/ThunarBookmarks/03ba78d592251e3504f5dac793790448" "")
